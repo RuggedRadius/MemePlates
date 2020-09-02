3b8651050be08b50658389d8a9ff76df75c92519
Ben Royans <ben.royans@gmail.com>
2020-09-02 15:45:57 +0800

    v1.1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,7 @@
-﻿using ColorPickerWPF;
-using Notifications.Wpf;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Reflection;
@@ -22,19 +22,75 @@ namespace MemePlates
     {
         // Globals
         
-        string[] acceptedFileTypes = { "*.png", "*.jpg", "*.jpeg", "*.bmp", "*.gif" };
-        List<Image> uiElements = new List<Image>();
+        string[] ACCEPTED_IMAGE_FORMATS = { "*.png", "*.jpg", "*.jpeg", "*.bmp", "*.gif" };
+        string[] ACCEPTED_VIDEO_FORMATS = { ".mp4" }; //"*.webm", 
+        List<Image> uiImageElements = new List<Image>();
+        List<MediaElement> uiVideoElements = new List<MediaElement>();
         List<WrapPanel> uiWrapPanels = new List<WrapPanel>();
 
-        NotificationManager toastMan = new NotificationManager();
+        System.Windows.Forms.NotifyIcon ni;
+
+        public string MEME_DIRECTORY_ROOT = "null";
+        private int totalMemes = 0;
+        private int totalSubDirs = 0;
 
         // Constructor
         public MainWindow()
         {
             InitializeComponent();
 
+            InitialiseTrayIcon();
+
             // Load images
             LoadMemes();
+
+            wndMain.KeyDown += WndMain_KeyDown;
+        }
+
+        private void InitialiseTrayIcon()
+        {
+            ni = new System.Windows.Forms.NotifyIcon();
+
+            // Tray Icon
+            ni.Icon = new Icon(Application.GetResourceStream(new Uri("pack://application:,,,/Resources/pepe.ico")).Stream);
+            ni.Visible = true;
+            ni.Click +=
+                delegate (object sender, EventArgs args)
+                {
+                    if (this.IsVisible)
+                    {
+                        this.Hide();
+                        Console.WriteLine("Hiding");
+                    }
+                    else
+                    {
+                        this.Show();
+                        wndMain.Activate();
+                        sliderImageSize.Focus();
+                        Console.WriteLine("Showing");
+                    }
+                };
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                this.Hide();
+
+            base.OnStateChanged(e);
+        }
+
+        private void WndMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.PageUp)
+            {
+                sliderImageSize.Value++;
+            }
+            if (e.Key == Key.PageDown)
+            {
+                sliderImageSize.Value--;
+            }
+            
         }
 
         private void LoadMemes()
@@ -43,24 +99,25 @@ namespace MemePlates
             List<DirectoryInfo> dirs = new List<DirectoryInfo>();
 
             // Get files
-            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Memes";
-            dirs.Add(new DirectoryInfo(path));
+            MEME_DIRECTORY_ROOT = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Memes";
+            dirs.Add(new DirectoryInfo(MEME_DIRECTORY_ROOT));
 
             // Check if folder initialisation is needed
-            if (!Directory.Exists(path))
+            if (!Directory.Exists(MEME_DIRECTORY_ROOT))
             {
                 MessageBox.Show("No folder");
-                Directory.CreateDirectory(path);
-                Directory.CreateDirectory(path + "\\Reactions");
-                Directory.CreateDirectory(path + "\\Rename me 1");
-                Directory.CreateDirectory(path + "\\Rename me 2");
-                File.Create(path + "\\put memes here.txt");
+                Directory.CreateDirectory(MEME_DIRECTORY_ROOT);
+                Directory.CreateDirectory(MEME_DIRECTORY_ROOT + "\\Reactions");
+                Directory.CreateDirectory(MEME_DIRECTORY_ROOT + "\\Rename me 1");
+                Directory.CreateDirectory(MEME_DIRECTORY_ROOT + "\\Rename me 2");
+                File.Create(MEME_DIRECTORY_ROOT + "\\put memes here.txt");
             }
 
             // Get sub-directories
             foreach (DirectoryInfo dir in dirs[0].GetDirectories())
             {
                 dirs.Add(dir);
+                
             }
 
             // For each found directory...
@@ -92,35 +149,43 @@ namespace MemePlates
                 // Set tab contents
                 newTab.Content = newScroller;
 
-                // Add all accepted file types
-                foreach (string filetype in acceptedFileTypes)
+                totalSubDirs++;
+
+                // Add all accepted image types
+                foreach (string filetype in ACCEPTED_IMAGE_FORMATS)
+                {
+                    FileInfo[] files = dir.GetFiles(filetype);
+                    foreach (FileInfo file in files)
+                    {
+                        newPanel.Children.Add(CreateImageElement(file));
+                        totalMemes++;
+                    }
+                }
+
+                // Add all accepted videos
+                foreach (string filetype in ACCEPTED_VIDEO_FORMATS)
                 {
                     FileInfo[] files = dir.GetFiles(filetype);
                     foreach (FileInfo file in files)
                     {
                         // Add UI element
-                        newPanel.Children.Add(CreateUIElement(file));
+                        newPanel.Children.Add(CreateVideoElement(file));
+
+                        totalMemes++;
                     }
                 }
+
             }
+
+            // Update labels
+            lblTotalMemeCount.Content = string.Format("{0} Memes", totalMemes.ToString());
+            lblTotalSubDirsCount.Content = string.Format("{0} Folders", totalSubDirs.ToString());
         }
 
         private void RefreshMemes()
         {
-            if (miToastsEnabled.IsChecked)
-            {
-                // Toast
-                toastMan.Show(new NotificationContent
-                {
-                    Title = "Refreshing memes",
-                    Message = "Refreshing your memes now...",
-                    Type = NotificationType.Information
-                });
-            }
-
-
             // Clear all UI elements
-            uiElements.Clear();
+            uiImageElements.Clear();
             uiWrapPanels.Clear();
             tabMain.Items.Clear();
 
@@ -128,29 +193,149 @@ namespace MemePlates
             LoadMemes();
         }
 
-        private Image CreateUIElement(FileInfo file)
+        //private Image CreateGIFElement(FileInfo file)
+        //{
+        //    // Create UI element from bitmap image source
+        //    var gifMeme = new BitmapImage();
+        //    gifMeme.BeginInit();
+        //    gifMeme.UriSource = new Uri(file.FullName);
+        //    gifMeme.EndInit();
+
+        //    // Set animated source
+        //    Image newImg = new Image();
+        //    ImageBehavior.SetAnimatedSource(newImg, gifMeme);
+
+        //    // Set size
+        //    newImg.Width = CalculateImageWidth();
+        //    newImg.Height = Double.NaN;
+
+        //    // Add location to tag
+        //    newImg.Tag = file.FullName;
+
+        //    // Add handler
+        //    newImg.MouseDown += gifImage_MouseDown;
+
+
+
+
+
+
+        //    // Add to list
+        //    uiImageElements.Add(newImg);
+
+        //    return newImg;
+        //}
+
+        private void gifImage_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            // Create UI element from bitmap image source
-            Image uiElement = new Image();
-            uiElement.Source = new BitmapImage(new Uri(file.FullName));
+            // Get clicked image
+            Image uiElementClicked = (Image)sender;
 
-            // Set size
-            uiElement.Width = CalculateImageWidth();
-            uiElement.Height = Double.NaN;
+            // Get path from tag
+            string path = uiElementClicked.Tag.ToString();
 
-            // Add location to tag
-            uiElement.Tag = file.FullName;
+            StringCollection paths = new StringCollection();
+            paths.Add(path);
 
-            // Add handler
-            uiElement.MouseDown += UiElement_MouseDown;
+            // add to clipboard
+            Clipboard.Clear();
+            Clipboard.SetFileDropList(paths);
 
-            // Add to list
-            uiElements.Add(uiElement);
-
-            return uiElement;
+            if (chkNotificationsOn.IsChecked == true)
+            {
+                // Toast
+                ni.ShowBalloonTip(2, "", "Copied to clipboard.", System.Windows.Forms.ToolTipIcon.None);
+            }
         }
 
-        private void UiElement_MouseDown(object sender, MouseButtonEventArgs e)
+        private MediaElement CreateVideoElement(FileInfo file)
+        {
+            // Create UI element from bitmap image source
+            MediaElement videoMeme = new MediaElement();
+            videoMeme.Source = new Uri(file.FullName);
+
+            // Set size
+            videoMeme.Width = CalculateImageWidth();
+            videoMeme.Height = Double.NaN;
+
+            // Add location to tag
+            videoMeme.Tag = file.FullName;
+
+            // Add handler
+            videoMeme.MouseDown += UiVideoElement_MouseDown;
+            videoMeme.MouseEnter += VideoMeme_MouseEnter;
+            videoMeme.MouseLeave += VideoMeme_MouseLeave; ;
+
+            // Mute videos
+            videoMeme.IsMuted = true;
+            videoMeme.MediaEnded += VideoMeme_MediaEnded;
+            videoMeme.UnloadedBehavior = MediaState.Stop;
+            videoMeme.LoadedBehavior = MediaState.Manual;
+
+            // Add to list
+            uiVideoElements.Add(videoMeme);
+
+            return videoMeme;
+        }
+
+        private void VideoMeme_MouseLeave(object sender, MouseEventArgs e)
+        {
+            MediaElement video = (MediaElement)sender;
+            video.Play();
+        }
+
+        private void VideoMeme_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MediaElement video = (MediaElement)sender;
+            video.Stop();
+        }
+
+        private void VideoMeme_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            MediaElement vid = (MediaElement)e.Source;
+            vid.Play();
+        }
+
+        private Image CreateImageElement(FileInfo file)
+        {
+            // Create UI element from bitmap image source
+            Image imageMeme = new Image();
+            imageMeme.Source = new BitmapImage(new Uri(file.FullName));
+
+            // Set size
+            imageMeme.Width = CalculateImageWidth();
+            imageMeme.Height = Double.NaN;
+
+            // Add location to tag
+            imageMeme.Tag = file.FullName;
+
+            // Add handler
+            imageMeme.MouseDown += UiImageElement_MouseDown; 
+
+            
+
+            // Add to list
+            uiImageElements.Add(imageMeme);
+
+            return imageMeme;
+        }
+
+        private void UiVideoElement_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //// add to clipboard
+            //StringCollection paths = new StringCollection();
+            //paths.Add(((MediaElement)sender).Tag.ToString());
+            //Clipboard.Clear();
+            //Clipboard.SetFileDropList(paths);
+
+            if (chkNotificationsOn.IsChecked == true)
+            {
+                // Toast
+                ni.ShowBalloonTip(2, "Cannot copy to clipboard", "Format not supported yet.", System.Windows.Forms.ToolTipIcon.Error);
+            }
+        }
+
+        private void UiImageElement_MouseDown(object sender, MouseButtonEventArgs e)
         {
             // Get clicked image
             Image uiElementClicked = (Image)sender;
@@ -169,32 +354,34 @@ namespace MemePlates
             Clipboard.Clear();
             Clipboard.SetImage(img);
 
-            if (miToastsEnabled.IsChecked)
+            if (chkNotificationsOn.IsChecked == true)
             {
                 // Toast
-                toastMan.Show(new NotificationContent
-                {
-                    Title = "Copied to clipboard!",
-                    Message = "Meme cannon loaded",
-                    Type = NotificationType.Success
-                });
+                ni.ShowBalloonTip(2, "", "Copied to clipboard.", System.Windows.Forms.ToolTipIcon.None);
             }
         }
 
         private void sliderImageSize_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             // Resize all images
-            foreach (Image img in uiElements)
+            foreach (Image img in uiImageElements)
             {
                 img.Width = CalculateImageWidth();
                 img.Height = Double.NaN;
             }
 
-            // Resize all wrap panel items
+            // Resize all wrap panels
             foreach (WrapPanel wrap in uiWrapPanels)
             {
                 wrap.ItemWidth = CalculateImageWidth();
                 wrap.ItemHeight = Double.NaN;
+            }
+
+            // Resize all videos
+            foreach (MediaElement video in uiVideoElements)
+            {
+                video.Width = CalculateImageWidth();
+                video.Height = Double.NaN;
             }
         }
 
@@ -208,40 +395,31 @@ namespace MemePlates
             Environment.Exit(0);
         }
 
-        private void miRefresh_Click(object sender, RoutedEventArgs e)
+        private void miToastsEnabled_Click(object sender, RoutedEventArgs e)
+        {
+            chkNotificationsOn.IsChecked = !chkNotificationsOn.IsChecked;
+        }
+
+        private void btnRescanMemesDir_Click(object sender, RoutedEventArgs e)
         {
             RefreshMemes();
         }
 
-        private void miToastsEnabled_Click(object sender, RoutedEventArgs e)
+        private void btnOpenMemesDir_Click(object sender, RoutedEventArgs e)
         {
-            miToastsEnabled.IsChecked = !miToastsEnabled.IsChecked;
+            Process.Start(MEME_DIRECTORY_ROOT);
         }
 
-        private void miBackgroundColour_Click(object sender, RoutedEventArgs e)
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            System.Windows.Media.Color color;
+            this.Hide();
+            e.Cancel = true;
+            return;
+        }
 
-            bool ok = ColorPickerWindow.ShowDialog(out color);
+        private void wndMain_Closed(object sender, EventArgs e)
+        {
 
-            if (ok)
-            {
-                RadialGradientBrush bg = new RadialGradientBrush();
-                bg.GradientOrigin = new System.Windows.Point(0.5, 0.5);
-                bg.Center = new System.Windows.Point(0.5, 0.5);
-
-                GradientStop inner = new GradientStop();
-                inner.Color = color;
-                inner.Offset = 0.5;
-                bg.GradientStops.Add(inner);
-
-                GradientStop outer = new GradientStop();
-                outer.Color = Colors.Black;
-                outer.Offset = 1;
-                bg.GradientStops.Add(outer);
-
-                dpMaster.Background = bg;
-            }
         }
     }
 }
